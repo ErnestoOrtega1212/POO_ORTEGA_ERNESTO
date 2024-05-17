56ee30edbc248628e2d6bb580323a0fa4dc51c8e
Ernesto Ortega <ernesto_3141230036@utd.edu.mx>
2024-05-17 12:59:13 -0600

    programa 3 - imc

--- programa3.py
+++ programa3.py
@@ -1,8 +1,8 @@
-peso = float(input("Ingrese su peso (kg): "))
+peso = float(input("Ingrese su peso en (kg): "))
 
-estatura = float(input("Ingrese su estatura (m): "))
+estatura = float(input("Ingrese su estatura en (m): "))
 
 imc = round(peso / (estatura ** 2), 2)
 
-print("Su indice de masa corporal es: ", imc)
+print("Su índice de masa corporal es: ", imc)
 
--- programa4.py
+++ programa4.py
@@ -1,10 +1,10 @@
 
-a = 2
-b = 4 
-c = -6
+a = int(input("Ingrese el valor de a: "))
+b = int(input("Ingrese el valor de b: "))
+c = int(input("Ingrese el valor de c: "))
 
 x1 = (-b +  (b**2 - (4 * a * c)) ** 0.5) / (2 * a)
 x2 = (-b -  (b**2 - (4 * a * c)) ** 0.5) / (2 * a)
 
-print(x1)
-print(x2)
+print("El valor  de x1 es: ", x1)
+print("El valor de x2 es: ", x2)
