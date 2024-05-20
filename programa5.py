0f505d0a6fb34abb62e695fff45e862ff3143850
Ernesto Ortega <ernesto_3141230036@utd.edu.mx>
2024-05-20 11:32:59 -0600

    programa 5 - Mayor de edad

---
+++ programa5.py
@@ -0,0 +1,7 @@
+print("Bienvenido al antro ""x")
+edad = int(input("Ingrese su edad: "))
+
+if edad >= 18:
+    print("Puedes ingresar al antro")
+else:
+    print("No puedes ingresar al antro, eres menor de edad")
