195c5ce8596121b70abd333df3000faf14c98b04
Ernesto Ortega <ernesto_3141230036@utd.edu.mx>
2024-05-20 11:48:43 -0600

    programa 6 - Numero mayor

---
+++ programa6.py
@@ -0,0 +1,11 @@
+usuario1 = float(input("Ingrese un valor numerico cualquiera: "))
+usuario2 = float(input("Ingrese un segundo valor numerico: "))
+
+if usuario1 > usuario2:
+    print("El numero", usuario1, "es mayor que", usuario2)
+elif usuario1 < usuario2:
+    print("El numero", usuario2, "es mayor que", usuario1)
+
+else:
+    print("el numero", usuario2, "es igual que", usuario1)
+    
