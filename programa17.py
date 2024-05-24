0ee982e83c92c0c4b5cbcc78f434f454d4c4559d
Ernesto Ortega <ernesto_3141230036@utd.edu.mx>
2024-05-24 13:03:22 -0600

    Programa 17 - Conversion de temperaturas

---
+++ programa17.py
@@ -0,0 +1,24 @@
+
+while True:
+    print("Que conversion de temperatura quieres haacer? ")
+    print("a) Celcius a Farenheit")
+    print("b) Farenheit a Celcius")
+    print("c) Cerrar programa \n")
+    print("Ingresa de manera correcta el inciso\n")
+
+    eleccion = str(input())
+
+    if eleccion == "a":
+        usuario = float(input("Ingrese los Celsius que decida convertir: \n"))        
+        Farenheit = (usuario * 9 / 5) + 32
+        print(Farenheit)
+    
+    elif eleccion == "b":
+        usuario = float(input("Ingrese los Farenheit que quiera convertir: \n"))
+        Celcius = (usuario - 32) * 5 / 9
+        print(Celcius)
+    
+    elif eleccion == "c":
+        break
+    else:
+        print("No ingresaste una opcion dentro del menú")
