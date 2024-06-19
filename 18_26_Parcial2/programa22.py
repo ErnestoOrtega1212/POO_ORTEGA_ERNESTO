1812e57886ed60718e9e16920595af1db276a92c
Ernesto Ortega <ernesto_3141230036@utd.edu.mx>
2024-06-19 07:48:40 -0600

    Programa 22 - Calculadora Basica con Funciones

---
+++ 18_26_Parcial2/programa22.py
@@ -0,0 +1,14 @@
+def sumar(a, b):
+    sumar = a + b
+
+def restar(a, b):
+    restar = a - b
+
+def multiplicar(a, b):
+    multi = a * b
+
+def dividir(a, b):
+    div = a * b
+
+
+print("Escoge lo que quieres hacer: ")
